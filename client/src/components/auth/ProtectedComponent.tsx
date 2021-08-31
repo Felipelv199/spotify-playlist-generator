af4933dbc52d8cb70a368be81575eba7e79900f9
Felipe López Valdez <0204021@up.edu.mx>
2021-08-30 23:18:57 -0500

    Add Protected Component file

---
+++ client/src/components/auth/ProtectedComponent.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/jsx-curly-newline */
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable no-confusing-arrow */
+/* eslint-disable implicit-arrow-linebreak */
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { State } from '../../state';
+
+interface ProtectedComponentProps {
+  Component: React.ComponentType<any>;
+  exact: boolean;
+  path: string;
+}
+
+const ProtectedComponent = (restOfProps: ProtectedComponentProps) => {
+  const auth = useSelector((state: State) => state.auth);
+  const { Component, exact, path } = restOfProps;
+  return (
+    <Route
+      exact={exact}
+      path={path}
+      render={(props) =>
+        auth ? <Component {...props} /> : <Redirect to="/" />
+      }
+    />
+  );
+};
+
+export default ProtectedComponent;
